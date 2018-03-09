2247499f4a1ff3f95566f36bf8777305d934072b
Chaitya62 <Chaitya62@gmail.com>
2018-03-09 23:05:03 +0530

    initial commit

---
+++ src/js/components/AppRoutes.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
+const AppRoutes = (props) => {
+  return (
+    <div>
+      <Route exact path='/' component={Error : no source code supplied to jspretty !}/>
+      <Route path='/' component={Error : no source code supplied to jspretty !}/>
+    </div>
+  );
+}
+
+export default AppRoutes;
